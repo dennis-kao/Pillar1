2922ddcee6b302c00db6a2e7db433d5ffb4a950d
James Mosley <james.george.mosley@gmail.com>
2017-11-20 22:59:57 -0500

    basic call works

---
+++ app/src/main/java/james/mosley/com/pillar/PillarAPI.java
@@ -0,0 +1,47 @@
+package james.mosley.com.pillar;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+/**
+ * Created by mosleyj on 2017-11-18.
+ */
+
+public class PillarAPI {
+    private OkHttpClient client = new OkHttpClient.Builder()
+            .retryOnConnectionFailure(true)
+            .build();
+
+
+    private String url1 = "https://131.104.180.41:8000/";
+    private String url2 = "https://www.google.com";
+    private String url3 = "http://nicholaswebsitewebs-env.us-east-1.elasticbeanstalk.com/email.php";
+    private String url4 = "http://131.104.180.41:8000/";
+
+    public void testCall() {
+        Request request = new Request.Builder()
+                .url(url1)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("testCall",e.toString());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                System.out.println(response.toString());
+            }
+        });
+    }
+
+}
