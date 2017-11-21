3118170bfa49c88cb98dde6332ef8d7cb39e5bd4
James Mosley <james.george.mosley@gmail.com>
2017-11-21 04:03:37 -0500

    Need to test to see what happens when invalid info is asked for.......

--- app/src/main/java/james/mosley/com/pillar/MainActivity.java
+++ app/src/main/java/james/mosley/com/pillar/MainActivity.java
@@ -1,9 +1,12 @@
 package james.mosley.com.pillar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import james.mosley.com.pillar.Models.LoginObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,14 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Testing Dev Branch");
 
         User client = new User();
         Medication med = new Medication();
         Community friends = new Community();
-        PillarAPI pillarAPI = new PillarAPI();
 
+        //Testing API calls... Won't be in this activity for final version
+        PillarAPI pillarAPI = new PillarAPI();
         pillarAPI.testCall();
+        try {
+            Boolean status = pillarAPI.createAccount("test@gmail.com", "password", "CLC Client");
+        } catch (Exception e) {
+            Log.e("createAccountError",e.getMessage());
+        }
+
+        try {
+            LoginObject temp = pillarAPI.login("God","thing");
+        } catch (Exception e) {
+            Log.e("LoginError",e.getMessage());
+        }
+
 
         //sets up recycler view
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
--- app/src/main/java/james/mosley/com/pillar/PillarAPI.java
+++ app/src/main/java/james/mosley/com/pillar/PillarAPI.java
@@ -1,13 +1,22 @@
 package james.mosley.com.pillar;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
-import java.io.IOException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import james.mosley.com.pillar.Models.LoginObject;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 
@@ -20,13 +29,16 @@ public class PillarAPI {
             .retryOnConnectionFailure(true)
             .build();
 
-
-    private String url1 = "http://131.104.180.41:8000/";
+    private Boolean done;
+    private Boolean result;
+    private JSONObject toReturn;
+    private String baseURL = "http://131.104.180.41:8000/";
+    private MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     public void testCall() {
         
         Request request = new Request.Builder()
-                .url(url1)
+                .url(baseURL)
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
@@ -42,4 +54,107 @@ public class PillarAPI {
         });
     }
 
+    public Boolean createAccount(String email, String password, String accountType) throws Exception {
+        done = false;
+        result = false;
+        toReturn = null;
+
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+        params.put("accountType",accountType);
+        JSONObject jsonObject = new JSONObject(params);
+
+        RequestBody body = RequestBody.create(JSON,jsonObject.toString());
+        Request request = new Request.Builder()
+                .url(baseURL+"users/login")
+                .post(body)
+                .header("Content-Type", "application/json; charset=utf-8")
+                .build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("createAccountFailure",e.toString());
+                result = false;
+                done = true;
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.i("CreateAccountCall","Login Successful!");
+                try {
+                    String resp = response.body().string();
+                    JSONObject jsonObject = new JSONObject(resp);
+                    result = true;
+                    done = true;
+                }catch (Exception e){
+                    Log.e("CreateAccountCall",e.getMessage());
+                }
+            }
+        });
+        while (!done);
+        return result;
+    }
+
+    public LoginObject login(String email, String password) {
+        result = false;
+        done = false;
+        toReturn = null;
+
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+        JSONObject jsonObject = new JSONObject(params);
+
+        RequestBody body = RequestBody.create(JSON,jsonObject.toString());
+        Request request = new Request.Builder()
+                .url(baseURL+"users/login")
+                .post(body)
+                .header("Content-Type", "application/json; charset=utf-8")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, IOException e) {
+                Log.e("LoginFailure",e.toString());
+                result = false;
+                done = true;
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                try {
+                    Log.i("LoginSuccess","Login Successful!");
+                    String resp = response.body().string();
+                    toReturn = new JSONObject(resp);
+                }catch (Exception e) {
+                    Log.e("LoginResponseParseError",e.getMessage());
+                }
+                result = true;
+                done = true;
+            }
+        });
+        while (!done);
+
+        String error = null;
+        String userID = null;
+        String loginToken = null;
+        String loginTokenExpires = null;
+        String accountType = null;
+
+        try {
+            userID = toReturn.getString("id");
+            loginToken = toReturn.getString("loginToken");
+            loginTokenExpires = toReturn.getString("loginTokenExpires");
+            accountType = toReturn.getString("accountType");
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        LoginObject loginObject = new LoginObject(error,userID,loginToken,loginTokenExpires,accountType);
+
+        return loginObject;
+    }
 }
